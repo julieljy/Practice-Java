d652368dabe2cce00cbc3c198bb7c34c0ae8bb38
julieljy <julieljy0328@gmail.com>
2016-01-07 16:21:43 +0800

    finish practice 5

--- src/main/java/Worker.java
+++ src/main/java/Worker.java
@@ -6,6 +6,6 @@ public class Worker extends Person{
         super(name,age);
     }
     public String introduce(){
-        return "I am a Worker. I have a job.";
+        return super.basicIntroduce()+"I am a Worker. I have a job.";
     }
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -10,8 +10,6 @@ public class StudentTest {
     @Test
     public void should_introduce_with_name_age_and_klass(){
         Student tom = new Student("Tom", 21, 2);
-        String basic = tom.basicIntroduce();
-        String student = tom.introduce();
-        assertThat(basic+student, is("My name is Tom. I am 21 years old. I am a Student. I am at Class 2."));
+        assertThat(tom.introduce(), is("My name is Tom. I am 21 years old. I am a Student. I am at Class 2."));
     }
 }
